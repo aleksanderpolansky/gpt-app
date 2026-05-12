86be09c5d651a5c580514cbe0aff67ec4871d723
Aleksander Polański <aleksanderpolansky@gmail.com>
2026-05-12 22:55:52 +0200

    Allow status rollback activity corrections

---
+++ supabase/migrations/018_activity_corrections_status_rollback.sql
@@ -0,0 +1,14 @@
+﻿alter table public.activity_corrections
+drop constraint if exists activity_corrections_correction_type_check;
+
+alter table public.activity_corrections
+add constraint activity_corrections_correction_type_check
+check (
+  correction_type in (
+    'timing_correction',
+    'duration_correction',
+    'comment_correction',
+    'manual_patch',
+    'status_rollback'
+  )
+);
